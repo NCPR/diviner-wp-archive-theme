46322dd69eaa6f63f55763bdaa38c191f4c4c72d
Graeme Hoffman <graeme@graemehoffman.com>
2018-05-02 08:27:34 -0400

    Plugin check

--- app/Setup/actions.php
+++ app/Setup/actions.php
@@ -23,3 +23,25 @@ function example_action()
     //
 }
 add_filter('excerpt_length', 'Tonik\Theme\App\Setup\example_action');
+
+/**
+ * Display notice when plugin is not installed.
+ *
+ * @return integer
+ */
+function plugin_check() {
+	if ( ! is_plugin_active( 'diviner-wp-archive-plugin/diviner-wp-archive-plugin.php' ) ){
+		?>
+		<div class="notice notice-success is-dismissible">
+			<p><?php _e('The Diviner Archive Theme requires the diviner-wp-archive-plugin Plugin', 'shapeSpace'); ?></p>
+		</div>
+		<?php
+	}
+}
+add_action('admin_notices', 'Tonik\Theme\App\Setup\plugin_check');
+
+
+/*
+ *
+ *
+ */
