764e32d98717e4967da28e6090fbc4776c1938f5
Graeme Hoffman <graeme@graemehoffman.com>
2018-05-08 08:25:14 -0400

    Better check for plugin

--- app/Setup/actions.php
+++ app/Setup/actions.php
@@ -30,18 +30,18 @@ add_filter('excerpt_length', 'Tonik\Theme\App\Setup\example_action');
  * @return integer
  */
 function plugin_check() {
+	$notice = sprintf(
+		__( 'The Diviner Archive Theme requires the diviner-wp-archive-plugin Plugin. Get it <a href="%s">here</a>.', 'diviner-archive'),
+		'https://github.com/NCPR/diviner-wp-archive-plugin'
+		);
+
 	if ( ! is_plugin_active( 'diviner-wp-archive-plugin/diviner-wp-archive-plugin.php' ) ){
 		?>
 		<div class="notice notice-success is-dismissible">
-			<p><?php _e('The Diviner Archive Theme requires the diviner-wp-archive-plugin Plugin', 'shapeSpace'); ?></p>
+			<p><?php echo $notice; ?></p>
 		</div>
 		<?php
 	}
 }
 add_action('admin_notices', 'Tonik\Theme\App\Setup\plugin_check');
 
-
-/*
- *
- *
- */
