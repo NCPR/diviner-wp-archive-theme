48c33256eda6e0b7c487a626d930e250f40909df
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-07-04 08:06:41 +0200

    apply WordPress code style

--- functions.php
+++ functions.php
@@ -13,22 +13,29 @@
  |
  */
 
+/**
+ * This is the theme entry point, if we are here then Diviner
+ * is being used as a theme.
+ */
+define( 'DIVINER_IS_PLUGIN', false );
+define( 'DIVINER_IS_THEME', true );
+
 // Require Composer's autoloading file
 // if it's present in theme directory.
-if (file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
-    require $composer;
+if ( file_exists( $composer = __DIR__ . '/vendor/autoload.php' ) ) {
+	require $composer;
 }
 
 // Before running we need to check if everything is in place.
 // If something went wrong, we will display friendly alert.
-$ok = require_once __DIR__ . '/bootstrap/compatibility.php';
+$ok = require __DIR__ . '/bootstrap/compatibility.php';
 
-if ($ok) {
-    // Now, we can bootstrap our theme.
-    $theme = require_once __DIR__ . '/bootstrap/theme.php';
+if ( $ok ) {
+	// Now, we can bootstrap our theme.
+	$theme = require __DIR__ . '/bootstrap/theme.php';
 
-    // Autoload theme. Uses localize_template() and
-    // supports child theme overriding. However,
-    // they must be under the same dir path.
-    (new Tonik\Gin\Foundation\Autoloader($theme->get('config')))->register();
+	// Autoload theme. Uses localize_template() and
+	// supports child theme overriding. However,
+	// they must be under the same dir path.
+	( new Tonik\Gin\Foundation\Autoloader( $theme->get( 'config' ) ) )->register();
 }
