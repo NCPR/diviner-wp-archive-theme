34d3a90d18df243e7e44de263655ba19206d4b84
Graeme Hoffman <graeme@graemehoffman.com>
2019-03-13 15:59:10 +0100

    Image size definition tweaks

--- app/Config/General.php
+++ app/Config/General.php
@@ -15,8 +15,9 @@ class General {
 
 	const VERSION = '1.0.0';
 
-	const IMAGE_SIZE_BROWSE_POPUP = 'image_size_browse_popup'; // 5 x 1
-	const IMAGE_SIZE_FEATURE_SM   = 'image_size_feature_sm';
+	const IMAGE_SIZE_BROWSE_POPUP = 'image_size_browse_popup';
+
+	const IMAGE_SIZE_FEATURE_SM   = 'image_size_feature_sm'; // 5 x 1
 	const IMAGE_SIZE_FEATURE_MD   = 'image_size_feature_md';
 	const IMAGE_SIZE_FEATURE_LRG  = 'image_size_feature_lrg';
 
