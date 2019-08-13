49f3c006d22f14776ddcebfc0c400e44d3758ea8
Graeme Hoffman <graeme@graemehoffman.com>
2019-08-13 00:47:19 -0400

    do not display post date on search returns

--- resources/templates/partials/loop/content.tpl.php
+++ resources/templates/partials/loop/content.tpl.php
@@ -31,11 +31,13 @@ use \Diviner\Config\General;
 			<a href="<?php the_permalink(); ?>">
 				<h2 class="loop-item__title h3"><?php the_title(); ?></h2>
 			</a>
+			<?php if (get_post_type() === 'post') { ?>
 			<div class="loop-item__meta">
 				<time class="loop-item__date">
 					<?php the_time( get_option( 'date_format' ) ); ?>
 				</time>
 			</div>
+			<?php } ?>
 
 			<div class="loop-item__excerpt d-content">
 				<?php the_excerpt(); ?>
