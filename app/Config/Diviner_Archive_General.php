e1e6e92b08dce0ea2ab1ca2da86900dc93f48318
Graeme Hoffman <graeme@graemehoffman.com>
2019-08-13 15:49:32 -0400

    Version bump

--- app/Config/Diviner_Archive_General.php
+++ app/Config/Diviner_Archive_General.php
@@ -9,7 +9,7 @@ namespace Diviner_Archive\Config;
  */
 class Diviner_Archive_General {
 
-	const VERSION = '0.5.3';
+	const VERSION = '0.5.4';
 
 	const IMAGE_SIZE_BROWSE_POPUP = 'diviner_archive_image_size_browse_popup';
 	const IMAGE_SIZE_BROWSE_GRID = 'diviner_archive_image_size_browse_grid';
